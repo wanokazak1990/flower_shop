192840b1caf3224caebd1418636ce158b01d2297
Egleman <martsnov@hotmail.com>
2023-12-05 10:50:46 +0300

    вёрстка админки

---
+++ app/resources/js/src/components/adminComponents/CategoryList/CategoryList.jsx
@@ -0,0 +1,20 @@
+// export const CategoryList = () => {
+//     return (
+//         <div class="admin__products">
+//             <div class="admin__title">Список категорий</div>
+//             <ul class="admin__category-list">
+//                 <li v-for="category in categories">
+//                     <p>{{ category.name }}</p>
+//                     <div>
+//                         <va-button icon-color="#812E9E">Изменить</va-button>
+//                         <va-button color="danger" icon-color="#fff">Удалить</va-button>
+//                     </div>
+//                 </li>
+//                 <li v-if="categories.length === 0">
+//                     <p>Список категорий пуст</p>
+//                 </li>
+//             </ul>
+//             <Spinner class="spinner_absolute"/>
+//         </div>
+//     );
+// }
