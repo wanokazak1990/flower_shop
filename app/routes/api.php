f46e0e7f6b0e5dcbb8116d4c7d6cf60187db2745
oven-auto <oit@oven-auto.ru>
2023-12-05 10:48:08 +0300

    API DESCRIP

--- app/routes/api.php
+++ app/routes/api.php
@@ -19,8 +19,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::prefix('admin')->group(function(){
-    //Route::prefix('products')->group(function(){
-        Route::resource('categories',  \App\Http\Controllers\Api\V1\Category\CategoryController::class)->except(['edit', 'create']);
-        Route::resource('products',  \App\Http\Controllers\Api\V1\Product\ProductController::class)->except(['edit', 'create']);
-    //})
+    /**
+     * [GET]    [api/admin/categories]                      GET ALL CATEGORIES
+     * [GET]    [api/admin/categories/{int category_id}]    GET CATEGORY BY ID
+     * [POST]   [api/admin/categories/]                     STORE CATEGORY [name = string, sort = ?int]
+     * [PATCH]  [api/admin/categories/{int category_id}]    UPDATE CATEGORY BY ID [name = string, sort = ?int]
+     * [DELETE] [api/admin/categories/{int category_id}]    DELETE CATEGORY
+     */
+    Route::resource('categories',  \App\Http\Controllers\Api\V1\Category\CategoryController::class)->except(['edit', 'create']);
+
+    /**
+     * [GET]    [api/admin/products]                     GET ALL PRODUCTS
+     * [GET]    [api/admin/products/{int product_id}]    GET PRODUCT BY ID
+     * [POST]   [api/admin/products/]                    STORE PRODUCT [name = string, sort = ?int]
+     * [PATCH]  [api/admin/products/{int product_id}]    UPDATE PRODUCT BY ID [name = string, sort = ?int]
+     * [DELETE] [api/admin/products/{int product_id}]    DELETE PRODUCT
+     */
+    Route::resource('products',  \App\Http\Controllers\Api\V1\Product\ProductController::class)->except(['edit', 'create']);
 });
